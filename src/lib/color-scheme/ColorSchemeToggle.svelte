6860ca040726a0e85c0aaf1de218c4627085c542
jeremt <taboada.jeremie@gmail.com>
2024-10-09 16:20:39 +0200

    feat: style toggle properly

---
+++ src/lib/color-scheme/ColorSchemeToggle.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Moon from '$lib/icons/Moon.svelte';
+    import Sun from '$lib/icons/Sun.svelte';
+    import {getColorScheme} from './color-scheme.svelte';
+
+    let colorScheme = getColorScheme();
+</script>
+
+<button class="icon" onclick={colorScheme.toggle}>
+    {#if colorScheme.value === 'dark'}
+        <Moon />
+    {:else}
+        <Sun />
+    {/if}
+</button>
--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -1,6 +1,6 @@
 export async function load({locals}) {
-    const companies = await locals.pb.collection('companies').getFullList();
-    const invoices = await locals.pb.collection('invoices').getFullList({expand: 'company_id,client_id,organization_id'});
+    const companies = await locals.pb_admin.collection('companies').getFullList();
+    const invoices = await locals.pb_admin.collection('invoices').getFullList({expand: 'company_id,client_id,organization_id'});
 
     return {
         invoices,
