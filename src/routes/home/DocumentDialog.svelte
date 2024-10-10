714d2e0fd01749c4bc3d261a4bf895535e453dd9
jeremt <taboada.jeremie@gmail.com>
2024-10-10 21:48:22 +0200

    feat: delete invoice lines

--- src/routes/home/DocumentDialog.svelte
+++ src/routes/home/DocumentDialog.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Cross from '$lib/icons/Cross.svelte';
     import Dialog from '$lib/widgets/Dialog.svelte';
     import {resize} from '$lib/helpers/resize';
     import {formatDate} from '$lib/helpers/formatDate';
@@ -75,19 +76,12 @@
     <div class="editor">
         <header>
             <button aria-label="Back" class="icon" onclick={() => (isOpen = false)}>
-                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M1.22179 16.7782C2.00279 17.5592 3.26915 17.5592 4.05016 16.7782L8.99992 11.8284L13.9498 16.7782C14.7308 17.5592 15.9971 17.5592 16.7782 16.7782C17.5592 15.9971 17.5592 14.7308 16.7782 13.9498L11.8284 8.99999L16.7781 4.05026C17.5591 3.26921 17.5591 2.00288 16.7781 1.22183C15.9971 0.440785 14.7307 0.440785 13.9497 1.22183L8.99998 6.17157L4.05022 1.22185C3.26921 0.440799 2.00285 0.440799 1.22179 1.22185C0.440781 2.0029 0.440781 3.26923 1.22179 4.05027L6.17155 9L1.22179 13.9498C0.44072 14.7308 0.44072 15.9971 1.22179 16.7782Z"
-                        fill="var(--color-fg)"
-                    />
-                </svg>
+                <Cross />
             </button>
             <input type="number" style:width="5rem" placeholder="600" />
             <label for="quantityLabel" style:font-size="1rem">€ /</label>
             <input id="quantityLabel" style:width="5rem" type="text" placeholder="Jours" />
-            <a role="button" style:margin-left="auto" href="/api/invoices/id/pdf">Téléchager</a>
+            <a role="button" style:margin-left="auto" href="/api/invoices/id/pdf">Télécharger</a>
             <button class="btn">Sauvegarder</button>
         </header>
         <div class="invoice" use:resize={onResize} style:--ratio={ratio} style:height="{height}px">
@@ -111,7 +105,7 @@
                 </div>
             {/if}
             <div class="infos">
-                <div class="title">Facture nº{invoice.number} : <input type="text" class="invisible" placeholder="Titre" value={invoice.name} /></div>
+                <div class="title">Facture nº{invoice.number} : <input type="text" class="invisible" placeholder="Titre" bind:value={invoice.name} /></div>
                 <table>
                     <thead>
                         <tr>
@@ -120,10 +114,15 @@
                         </tr>
                     </thead>
                     <tbody>
-                        {#each invoice.lines as { description, price }}
+                        {#each invoice.lines as line, i}
                             <tr>
-                                <td>{description}</td>
-                                <td>{price} €</td>
+                                <td><input bind:value={line.description} type="text" class="invisible editable-description" placeholder="Description" /></td>
+                                <td><div class="editable-price"><input bind:value={line.price} type="number" class="invisible" placeholder="500" /> €</div> </td>
+                                <td style:padding="0" style:border="none" style:width="{10 * ratio}px"
+                                    ><button class="remove-line icon" style:color="var(--color-fg-1)" onclick={() => invoice.lines.splice(i, 1)}
+                                        ><Cross --size="{ratio * 12}px" /></button
+                                    ></td
+                                >
                             </tr>
                         {/each}
                         <tr>
@@ -226,9 +225,12 @@
             }
         }
         & table {
-            width: 100%;
+            width: calc(100% + 40px * var(--ratio));
             & th,
             & td {
+                & input {
+                    font-size: calc(12px * var(--ratio));
+                }
                 padding: 0 calc(8px * var(--ratio));
                 height: calc(25px * var(--ratio));
                 border-width: calc(1px * var(--ratio));
@@ -247,7 +249,7 @@
         display: flex;
         align-items: center;
         & input[type='number'] {
-            width: 3.5rem;
+            width: 3.8rem;
         }
     }
 </style>
